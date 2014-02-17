e2b914286757781a6ff6523063cb9191040c023c
fangao <61322@LP-61322.casco.signal>
2014-02-17 16:39:00 +0800

    index

--- javascripts/config.js
+++ javascripts/config.js
@@ -2,5 +2,5 @@ var disqus_shortname = 'fangao';
 var hostbase = 'http://fangao.github.io';
 var githubname = 'fangao';
 var repos = 'fangao.github.io';
-var sitetitle = 'fangao';
+var sitetitle = '黑色信封';
 var rss = '';
