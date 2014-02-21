e7c3fac5eba96bc7a0066359a32d3ebe9aa843cc
fangao <61322@LP-61322.casco.signal>
2014-02-21 09:59:27 +0800

    weibo.js

---
+++ javascripts/WB-kg.js
@@ -0,0 +1,19 @@
+WB2.anyWhere(function(W){
+		W.widget.connectButton({
+			id: "wb_connect_btn",	
+			type:'3,2',
+			callback : {
+				login:function(o){	//登录后的回调函数
+					W.parseCMD("/statuses/friends.json", function(sResult, bStatus){
+					    alert(sResult);
+					},{
+					    method: 'post'
+					});
+					alert("login: "+o.screen_name)	
+				},
+				logout:function(){	//退出后的回调函数
+					alert('logout');
+				}
+			}
+		});
+	});
