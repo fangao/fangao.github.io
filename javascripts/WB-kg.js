393dc4432f9d39cee642542e948fe88824e19f03
fangao <61322@LP-61322.casco.signal>
2014-02-21 10:29:42 +0800

    weibo.js

--- javascripts/WB-kg.js
+++ javascripts/WB-kg.js
@@ -1,19 +1,24 @@
 WB2.anyWhere(function(W){
-		W.widget.connectButton({
-			id: "wb_connect_btn",	
-			type:'3,2',
-			callback : {
-				login:function(o){	//登录后的回调函数
-					W.parseCMD("/statuses/friends.json", function(sResult, bStatus){
-					    alert(sResult);
-					},{
-					    method: 'post'
-					});
-					alert("login: "+o.screen_name)	
-				},
-				logout:function(){	//退出后的回调函数
-					alert('logout');
-				}
-			}
-		});
+	W.parseCMD("/statuses/friends.json", function(sResult, bStatus){
+    alert(sResult);
+},{
+    method: 'post'
+});
+		// W.widget.connectButton({
+		// 	id: "wb_connect_btn",	
+		// 	type:'3,2',
+		// 	callback : {
+		// 		login:function(o){	//登录后的回调函数
+		// 			W.parseCMD("/statuses/friends.json", function(sResult, bStatus){
+		// 			    alert(sResult);
+		// 			},{
+		// 			    method: 'post'
+		// 			});
+		// 			alert("login: "+o.screen_name)	
+		// 		},
+		// 		logout:function(){	//退出后的回调函数
+		// 			alert('logout');
+		// 		}
+		// 	}
+		// });
 	});
